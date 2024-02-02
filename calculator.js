7766f06e8dbdb7628e18a8dd067db54e2d1fee86
bokpack <6uiwoom@gmail.com>
2024-02-02 17:06:10 +0900

    240202 innerHTML로 화면표시 추가

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
 console.log('Hello World!');
 function sum() {
     const inputElement = document.getElementById('sumInput');
-    console.log(inputElement)
+    const inputValue = inputElement.value;
+    console.log(inputValue)
+
+    const resultElement = document.getElementById('resultSum')
+    resultElement.innerHTML = ` ${inputValue}`
 }
