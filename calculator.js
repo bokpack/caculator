f4ead0f1c7ff9925cafe47cf5c645c1dbbd91ffd
bokpack <6uiwoom@gmail.com>
2024-02-15 16:14:00 +0900

    240215 안주값계산완료

--- calculator.js
+++ calculator.js
@@ -9,14 +9,28 @@ function sum() {
 }
 
 function alcoholSum() {
+    // 술 개수랑 가격 곱하기
     const inputBottle = document.getElementById('bottle').value
     const inputBottlePrice = document.getElementById('bottlePrice').value
 
     const inputAlcohol = inputBottle * inputBottlePrice
 
     console.log(inputAlcohol)
-    
+
+    // 술값 합계 나타내기
     const inputAlcoholSum = document.getElementById('resultAlcoholSum')
     inputAlcoholSum.innerHTML = `술값 합계 : ${inputAlcohol}`
+
+    // 안주 합계 나타내기
+    const inputSum = document.getElementById('sum').value
+
+    const inputSnack =  inputSum - inputAlcohol
+    console.log(inputSnack)
+
+    const inputSnackSum = document.getElementById('snackSum')
+    inputSnackSum.innerHTML = `안주 합계 : ${inputSnack}`
+    
+
+    
 
 }
