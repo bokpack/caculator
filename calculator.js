e389a08092e3a34ed79676eacc9e7d3f9322a7f9
bokpack <6uiwoom@gmail.com>
2024-02-15 15:10:23 +0900

    240215 술값 곱하기 완료

--- calculator.js
+++ calculator.js
@@ -1,9 +1,22 @@
 console.log('Hello World!');
 function sum() {
-    const inputElement = document.getElementById('sumInput');
-    const inputValue = inputElement.value;
-    console.log(inputValue)
+    const inputSum = document.getElementById('sum').value;
+    // const inputValue = inputElement.value;
+    console.log(inputSum)
 
-    const resultElement = document.getElementById('resultSum')
-    resultElement.innerHTML = ` ${inputValue}`
+    const inputAllSum = document.getElementById('resultSum')
+    inputAllSum.innerHTML = ` 합계 : ${inputSum}`
+}
+
+function alcoholSum() {
+    const inputBottle = document.getElementById('bottle').value
+    const inputBottlePrice = document.getElementById('bottlePrice').value
+
+    const inputAlcohol = inputBottle * inputBottlePrice
+
+    console.log(inputAlcohol)
+    
+    const inputAlcoholSum = document.getElementById('resultAlcoholSum')
+    inputAlcoholSum.innerHTML = `술값 합계 : ${inputAlcohol}`
+
 }
